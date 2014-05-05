a3c155f10d3d703d8270f1bc1b4b71ca37bf1f74
Hyubs Ursua <hyubs@H-MBA.local>
2014-05-06 05:09:24 +0800

    Rewrote plugin for v2.0.0 - $.fn.fallback allows you to modify the fallback function (e.g. $.fn.fallback = $.fn.html) - multiMode (can be “floodling” or “jquery”) lets you change floodling’s strategy in handling multi-selects and checkboxes - Values are safely escaped - data-floodling-type and $.fn.floodling.register() allows you to create custom flood functions

--- src/jquery.floodling.js
+++ src/jquery.floodling.js
@@ -1,130 +1,146 @@
-(function( $ ) {
-	$.fn.floodling = function() {
-		var parent, elem, val, names, elemSelector;
+(function ($) {
+	"use strict";
 
-		if(arguments.length == 1) {
-			if((typeof arguments[0] === 'object')&&(!(arguments[0] instanceof Array))) {
-				names = arguments[0];
-				parent = $(this);
+	var F = function () {
+		if (arguments.length === 2) {
+			var elem = arguments[0];
+			var val = arguments[1];
+			F._floodForm($(this), elem, val);
+			return $(this);
+		} else if (arguments.length === 1) {
+			if(
+				typeof arguments[0] === 'object' &&
+				!(arguments[0] instanceof Array)
+			) {
+				var obj = arguments[0];
+				F._floodForm($(this), obj);
+				return $(this);
+			} else {
+				var val = arguments[0];
+				return F._floodElement($(this), val);
 			}
-			else {
-				parent = $('body');
-				elem = $(this);
-				val = arguments[0];
-				name = elem.attr('name');
-				setValue(parent, elem, val, name);
-				return;
-			}
-		}
-		else if(arguments.length == 2) {
-			var obj = {};
-			obj[arguments[0]] = arguments[1];
-			names = obj;
-			parent = $(this);
-		}
-
-		for(var name in names) {
-			val = names[name];
-			elem = parent.find('[name="' + name + '"]');
-			setValue(parent, elem, val, name);
 		}
 	};
 
-	function setValue(parent, elem, val, name) {
-		if(typeof elem !== 'undefined' && elem.length > 0) {
-			var tag = elem.prop("tagName").toLowerCase();
-			if(tag == 'input') {
-				var type = elem.prop('type').toLowerCase();
+	F._floodForm = function ($form) {
+		if (arguments.length === 3) {
+			var obj = {};
+			obj[arguments[1]] = arguments[2];
+			return F._floodForm($form, obj);
+		} else {
+			var obj = arguments[1];
 
-				if(type == 'text' || type == 'password') {
-					elem.val(val);
-				}
-				else if(type == 'radio') {
-					parent.find('[name="' + name + '"][value="' + val + '"]').prop("checked", true);
-				}
-				else if(type == 'checkbox') {
-					var nameLen = name.length;
-					if(nameLen > 2 && name.substring(nameLen - 2, nameLen) == '[]') {
-						if(val instanceof Array) {
-							for (var i = 0; i < val.length; i++){
-								var cbox = parent.find('[name="' + name + '"][value="' + val[i] + '"]');
-								if(cbox.prop('checked')) {
-									cbox.prop('checked', false);
-								}
-								else {
-									cbox.prop('checked', true);
-								}
-							}
-						}
-						else {
-							var cbox = parent.find('[name="' + name + '"][value="' + val + '"]');
-							if(cbox.prop('checked')) {
-								cbox.prop('checked', false);
-							}
-							else {
-								cbox.prop('checked', true);
-							}
-						}
-					}
-					else {
-						if(val != false) {
-							elem.prop('checked', true);
-						}
-						else {
-							elem.prop('checked', false);
-						}
-					}
-				}
-				else if(type == 'image') {
-					elem.prop('src', val);
-				}
-				else { // submit, button, reset, date, color, etc.
-					elem.val(val);
-				}
-			}
-			else if(tag == 'button') {
-				elem.html(val);
-			}
-			else if(tag == 'textarea') {
-				elem.val(val);
-			}
-			else if(tag == 'select') {
-				if(elem.prop('multiple')) {
-					var selectVal = elem.val();
-					if(selectVal === null) {
-						selectVal = [];
-					}
-					else if(typeof selectVal === 'string') {
-						selectVal = [selectVal];
-					}
-					if(val instanceof Array) {
-						for (var i = 0; i < val.length; i++) {
-    						var ms = parent.find('[value="' + val[i] + '"]');
-							if(ms.prop('selected')==true){
-								ms.prop('selected', false);
-							}
-							else{
-								ms.prop('selected', true);
-							}
-						}
-					}
-					else {
-						var ms = parent.find('[value="' + val + '"]');
-						if(ms.prop('selected')==true) {
-							ms.prop('selected', false);
-						}
-						else {
-							ms.prop('selected', true);
-						}
-					}
-				}
-				else {
-					elem.val(val);
-				}
+			for (var elem in obj) {
+				var val = obj[elem];
+				var $elem = $(F._getElemSelector(elem), $form);
+				F._floodElement($elem, val);
 			}
 		}
-		else {
-			console.error('Element $("' + elem.selector +'") was not found.');
+	};
+
+	F._getElemSelector = function (str) {
+		if (typeof str === 'string') {
+			if (str[0] !== '.' && str[0] !== '#') {
+				return '[name="' + str + '"]';
+			} else {
+				return str;
+			}
+		} else {
+			// Anything else, we'll return it back. It may be a jQuery object.
+			return str;
 		}
-	}
+	};
+
+	F._floodElement = function (elem, val) {
+		var $elem = $(F._getElemSelector(elem));
+		return F._setValue($elem, val);
+	};
+
+	F._escapeValAttr = function (val) {
+		return (val + '').replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
+	};
+
+	F._setValue = function ($elem, val) {
+		var tag = ($elem.prop("tagName") + '').toLowerCase();
+		var floodlingType = $elem.attr('data-floodling-type');
+
+		if (floodlingType && typeof F._registry[floodlingType] === 'function') {
+			return fallback = F._registry[floodlingType].call($elem, val);
+		} else {
+			if (tag === 'input') {
+				var type = ($elem.attr("type") + '').toLowerCase();
+
+				if (type === 'radio') {
+					return $elem.filter('[value="' + F._escapeValAttr(val) + '"]').prop("checked", true);
+				}
+				else if(type === 'checkbox') {
+					if (F.multiMode === 'floodling') {
+						var name = $elem.attr('name');
+
+						if (name && name.length > 2 && name.substring(name.length - 2, name.length) == '[]') {
+							if (val instanceof Array) {
+								for (var i in val) {
+									var $cbox = $elem.filter('[value="' + F._escapeValAttr(val[i]) + '"]');
+
+									if ($cbox) {
+										$cbox.prop('checked', !$cbox.prop('checked'));
+									}
+								}
+							} else {
+								var $cbox = $('[value="' + F._escapeValAttr(val[i]) + '"]', $elem);
+								return $cbox.prop('checked', (val));
+							}
+						} else {
+							return $elem.prop('checked', (val));
+						}
+					} else {
+						return $elem.val(val);
+					}
+				} else if(type === 'image') {
+					return $elem.prop('src', val);
+				} else { // type = [text, password, submit, button, reset, date, color, etc.]
+					return $elem.val(val);
+				}
+			} else if(tag === 'button') {
+				return $elem.html(val);
+			}
+			else if(tag === 'textarea') {
+				return $elem.val(val);
+			} else if(tag === 'select') {
+				if (F.multiMode === 'floodling') {
+					if ($elem.prop('multiple')) {
+						if (typeof val === 'string') {
+							val = [val];
+						}
+
+						if (val instanceof Array) {
+							for (var i in val) {
+								var $option = $('[value="' + F._escapeValAttr(val[i]) + '"]', $elem);
+
+								if ($option) {
+									$option.prop('selected', !$option.prop('selected'));
+								}
+							}
+						}
+					} else {
+						return $elem.val(val);
+					}
+				} else {
+					return $elem.val(val);
+				}
+			} else {
+				return F.fallback.call($elem, val);
+			}
+		}
+	};
+
+	F.fallback  = $.fn.val;
+	F.multiMode = 'floodling';
+	F._registry = {};
+
+	F.register = function (name, func) {
+		F._registry[name] = func;
+	};
+
+	$.fn.floodling = F;
 })(jQuery);
